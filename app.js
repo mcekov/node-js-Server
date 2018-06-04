bc6aeb16260e0b1167be9ce34c2590e97cabf154
Momchi <mcekovphoto@gmail.com>
2018-06-04 12:13:44 +0300

    add partial for navbar

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-const path = require('path');
+
 const expressHandlebars = require('express-handlebars');
 
 app.engine('hbs', expressHandlebars({
-	defaultLayout: 'layout',
-	extname: 'hbs',
-	layoutsDir: path.join(__dirname, 'views')
+    defaultLayout: 'layout',
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views')
 }));
 
 app.set('view engine', 'hbs');
@@ -16,24 +17,25 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 
+//Routes
+app.get('/', (req, res) => {
+    return res.render('home/index', {
+    	title: 'Test Html Page'
+	});
+});
+
 
 app.listen(port, (err) => {
-	if(err) {
-		return console.log('Something happened', err);
-	}
+    if(err) {
+        return console.log('Something happened', err);
+    }
 
-	console.log(`Server is up on ${port}`);
+    console.log(`Server is up on ${port}`);
 });
 
 app.use((err, request, response, next) => {
-  // log the error, for now just console.log
-  console.log(err);
+    // log the error, for now just console.log
+    console.log(err);
 
-  response.status(500).send('Something broke!')
-});
-
-
-//Routes
-app.get('/', (req, res) => {
-  return res.render('home/index');
+    response.status(500).send('Something broke!')
 });
